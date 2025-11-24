008ffe9517f26a53fced5e46cd12183206cba813
choyunsung <choyunsung79@gmail.com>
2025-11-24 13:27:22 +0900

    Initial Commit

---
+++ SecureSignInClient/SecureSignInClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureSignInClientApp.swift
+//  SecureSignInClient
+//
+//  Created by yunsung on 11/24/25.
+//
+
+import SwiftUI
+
+@main
+struct SecureSignInClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SecureSignInClientTests/SecureSignInClientTests.swift
@@ -0,0 +1,17 @@
+//
+//  SecureSignInClientTests.swift
+//  SecureSignInClientTests
+//
+//  Created by yunsung on 11/24/25.
+//
+
+import Testing
+@testable import SecureSignInClient
+
+struct SecureSignInClientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
